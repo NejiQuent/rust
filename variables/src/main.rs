51da75f777073c1e1cc99355515aa28f7e9fc692
Quentin Depeisses <nejiquent@gmail.com>
2022-04-21 20:21:01 +0200

    variables: 1st commit Data types
    Signed-off-by: Quentin Depeisses <nejiquent@gmail.com>

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -9,14 +9,17 @@
 
 fn main() {
     /* Variables and mutability */
-    let x = 5;
-    let x = x + 1;
-    {
-    let x = x * 2;
-    println!("The value of x in the inner scope is: {}", x);
-    }
-    println!("The value of x is: {}", x);
-    let spaces = "   "; //The advantage to shadow the variable is that we can use a new type of variable.
-    let spaces = spaces.len(); //For ex, if we had used let mut, spaces.len will do an error because it attend number type and during declaration of let mut spaces... it's declared as a str.
-    println!("number of spaces: {} ", spaces);
+    // let x = 5;
+    // let x = x + 1;
+    // {
+    // let x = x * 2;
+    // println!("The value of x in the inner scope is: {}", x);
+    // }
+    // println!("The value of x is: {}", x);
+    // let spaces = "   "; //The advantage to shadow the variable is that we can use a new type of variable.
+    // let spaces = spaces.len(); //For ex, if we had used let mut, spaces.len will do an error because it attend number type and during declaration of let mut spaces... it's declared as a str.
+    // println!("number of spaces: {} ", spaces);
+
+    /* Data types */
+    let guess: u32 = "42".parse().expect("Not a number!");
 }
